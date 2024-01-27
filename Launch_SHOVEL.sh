be7abce8082d7be66447d1c8736fde0e68b34c96
Arthur <135703612+Alglior@users.noreply.github.com>
2024-01-27 18:53:11 +0100

    launcher for linux

---
+++ Launch_SHOVEL.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Afficher un message
+echo "Activation de l'environnement virtuel 'shovel_insee'..."
+
+# Activer l'environnement virtuel
+# Assurez-vous que le chemin vers l'environnement virtuel est correct
+source scripts/shovel_insee/bin/activate  # Utilisez activate pour bash
+
+echo "Environnement virtuel activé."
+
+# Exécuter start.py
+echo "Exécution de start.py..."
+# Changer de répertoire pour le dossier scripts
+cd scripts
+python start.py
+# Revenir au répertoire original
+cd ..
+
+echo "start.py a terminé son exécution."
+exit
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Afficher un message
+echo "Téléchargement de l'installateur Python 3.10..."
+# Télécharger l'installateur Python
+wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
+
+# Afficher un message
+echo "Installation de Python 3.10..."
+# Extraire l'archive
+tar -xzf Python-3.10.0.tgz
+cd Python-3.10.0
+
+# Configurer et installer Python
+./configure
+make
+sudo make install
+
+# Revenir au répertoire précédent
+cd ..
+
+echo "Python 3.10 installé avec succès."
+
+# Exécuter requirements.py
+echo "Exécution de requirements.py..."
+# Changer de répertoire pour le dossier scripts
+cd scripts
+python requirements.py
+# Revenir au répertoire original
+cd ..
+
+echo "requirements.py a terminé son exécution."
